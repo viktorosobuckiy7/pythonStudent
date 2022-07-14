5a4d2999f10381d2bfe1797ab28f5d1aebe1e449
viktorosobuckiy7 <106606795+viktorosobuckiy7@users.noreply.github.com>
2022-07-14 12:25:02 +0300

    Add files via upload

---
+++ сlosures.py
@@ -0,0 +1,43 @@
+# def a1(n1, n):
+#     count = 0
+#     index = 1
+#
+#     def a2(q):
+#         nonlocal index
+#         nonlocal count
+#         while count < n:
+#             yield index * q
+#             count += 1
+#             index += 1
+#     return a2
+#
+#
+# x = a1(20, 10)
+# y = x(5)
+# for i in y:
+#     if i > 10:
+#         y.close()
+#     print(i)
+
+# def fib():
+#     n1 = 0
+#     n2 = 1
+#
+#     def find_number():
+#         nonlocal n1
+#         nonlocal n2
+#         n3 = n1 + n2
+#         n1, n2 = n2, n3
+#         return n3
+#
+#     return find_number
+
+
+# def sum_numbers():
+#     numbers = []
+#
+#     def inner(number):
+#         if isinstance(number, int):
+#             numbers.append(number)
+#         return sum(numbers)
+#     return inner
